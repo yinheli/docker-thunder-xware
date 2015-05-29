f5dc64b2e8b293e8a1f6dc43f44eed09b8c982ff
yinheli <yinheli@users.noreply.github.com>
2015-05-29 16:56:15 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,13 +2,7 @@ if [ ! -d data ]; then
     mkdir -p data
 fi
 
-docker images | grep docker-xware
-if [ ! $? -eq 0 ]; then
-    docker build -t docker-xware .
-fi
-
 docker run -d --privileged=true \
         --name=xware \
         -v $(pwd)/data:/app/TDDOWNLOAD \
-        -v $(pwd)/thunder/bin:/app/bin \
-    docker-xware
+        yinheli/docker-thunder-xware:latest
